5b002a99779ff36ee2a8e07ea65aeee6b61d877e
Michael Reuter <mareuternh@gmail.com>
2014-04-10 19:12:57 -0400

    Refs #2. Have it working by hand now.

--- lct/ui/widgets/fraction_widget.py
+++ lct/ui/widgets/fraction_widget.py
@@ -14,32 +14,49 @@ class FractionWidget(QtGui.QWidget):
         Constructor
         '''
         super(FractionWidget, self).__init__(parent)
+        self.setMinimumSize(self.minimumSizeHint())
         
     def paintEvent(self, event=None):
+        import math
         logical_size = 100.0;
+        fraction = 0.0
+        waxing_phase = True
+        b0 = 1.0 - 2.0 * fraction
         
-        def logicalFromPhysical(length, side):
-            return (length / side) * logical_size
-        fm = QtGui.QFontMetricsF(self.font())
+        overlap_color = QtCore.Qt.black
+        if b0 < 0:
+            overlap_color = QtCore.Qt.white
         
         margin_width = logical_size * 0.05
         margin_height = logical_size * 0.05
+        half_width = logical_size / 2.0
         width = logical_size - (2.0 * margin_width)
         height = logical_size - (2.0 * margin_height)
         
         painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        side = min(self.width(), self.height())
+        painter.setViewport(QtCore.QRect((self.width() - side)/2, 
+                                         (self.height() - side)/2,
+                                         side, side))
         painter.setWindow(0, 0, logical_size, logical_size)
         painter.setPen(QtCore.Qt.black)
         painter.setBrush(QtCore.Qt.black)
         painter.drawEllipse(QtCore.QRectF(margin_width, margin_height, width, height))
         
         painter.setBrush(QtCore.Qt.white)
-        painter.drawChord(QtCore.QRectF(margin_width, margin_height, width, height), 
-            -90 * 16, 180 * 16)
+        painter.setPen(QtCore.Qt.NoPen)       
+        painter.drawChord(QtCore.QRectF(margin_width, margin_height, 
+                                        width, height), 
+                          (-1 if waxing_phase else 1) * 90 * 16, 180 * 16)
+
+        painter.setBrush(overlap_color)
+        illum_chord = math.fabs(b0) * width
+        painter.drawEllipse(QtCore.QRectF(half_width - 0.5 * illum_chord, margin_height, 
+                                          illum_chord, height))
         
-        painter.setBrush(QtCore.Qt.black)
-        painter.drawChord(QtCore.QRectF(logical_size/2.0, margin_height, (width / 2.0) * 0.28, height), 
-            -90 * 16, 180 * 16)
+    def minimumSizeHint(self):
+        return QtCore.QSize(200, 200)
         
 if __name__ == "__main__":
     import sys
