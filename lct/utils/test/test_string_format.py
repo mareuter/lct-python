6c41a95cb09c02c0d9584d0d1d5dd8af57892152
Michael Reuter <mareuternh@gmail.com>
2014-04-23 23:09:55 -0400

    Refs #12. Changing test module name.

---
+++ lct/utils/test/test_string_format.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 23, 2014
+
+@author: Michael
+'''
+import unittest
+import utils
+
+class StringFormatTest(unittest.TestCase):
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
