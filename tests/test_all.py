545388b3ac26d23d2d0685b8c14d88d9260b1862
Michael Reuter <mareuternh@gmail.com>
2014-05-08 21:33:35 -0400

    Refs #14. Putting tests in place.

---
+++ tests/test_all.py
@@ -0,0 +1,25 @@
+'''
+Created on May 7, 2014
+
+@author: Michael Reuter
+
+Run all test cases.
+'''
+import unittest
+
+def suite():
+    """
+    Make a test suite to run all the tests.
+    """
+    test_modules = [
+                    'tests.utils.test_converter',
+                    'tests.utils.test_string_format'
+                    ]
+    all_tests = unittest.TestSuite()
+    for name in test_modules:
+        exec('from %s import suite as test_suite' % name)
+        all_tests.addTest(test_suite())
+    return all_tests
+   
+if __name__ == "__main__":
+    unittest.TextTestRunner().run(suite())
--- tests/utils/test_string_format.py
+++ tests/utils/test_string_format.py
@@ -4,44 +4,55 @@ Created on Apr 23, 2014
 @author: Michael Reuter
 '''
 import unittest
-import constants
+
+import lct.utils.constants as constants
+from lct.utils.string_format import StrFmt
 
 class StringFormatTest(unittest.TestCase):
 
     def test_floatString(self):
         # 2-digits precision with negative number
-        self.assertEqual(constants.StrFmt.floatString(-0.32421, 2), "-0.32")
+        self.assertEqual(StrFmt.floatString(-0.32421, 2), "-0.32")
         # 2-digits precision with suffix on number
-        self.assertEqual(constants.StrFmt.floatString(1.57542, 2, " meters"), "1.58 meters")
+        self.assertEqual(StrFmt.floatString(1.57542, 2, " meters"), "1.58 meters")
     
     def test_ddString(self):
         # North latitude
-        self.assertEqual(constants.StrFmt.ddString(31.24654, 2, constants.LATITUDE), 
+        self.assertEqual(StrFmt.ddString(31.24654, 2, constants.LATITUDE), 
                          "31.25"+constants.DEGREE_MARKER+" N")
         # South latitude
-        self.assertEqual(constants.StrFmt.ddString(-31.24654, 2, constants.LATITUDE), 
+        self.assertEqual(StrFmt.ddString(-31.24654, 2, constants.LATITUDE), 
                          "31.25"+constants.DEGREE_MARKER+" S")
         # East longitude
-        self.assertEqual(constants.StrFmt.ddString(31.24654, 2, constants.LONGITUDE), 
+        self.assertEqual(StrFmt.ddString(31.24654, 2, constants.LONGITUDE), 
                          "31.25"+constants.DEGREE_MARKER+" E")
         # West longitude
-        self.assertEqual(constants.StrFmt.ddString(-31.24654, 2, constants.LONGITUDE), 
+        self.assertEqual(StrFmt.ddString(-31.24654, 2, constants.LONGITUDE), 
                          "31.25"+constants.DEGREE_MARKER+" W")
         
     def test_dmsString(self):
         # Positive DMS tuple
-        self.assertEqual(constants.StrFmt.dmsString((15, 30, 20, 1)), 
+        self.assertEqual(StrFmt.dmsString((15, 30, 20, 1)), 
                          "15"+constants.DEGREE_MARKER+" 30"+constants.ANGLE_MARKERS[1]+" 20"+constants.ANGLE_MARKERS[2])
         # Negative DMS tuple
-        self.assertEqual(constants.StrFmt.dmsString((15, 30, 20, -1)), 
+        self.assertEqual(StrFmt.dmsString((15, 30, 20, -1)), 
                          "-15"+constants.DEGREE_MARKER+" 30"+constants.ANGLE_MARKERS[1]+" 20"+constants.ANGLE_MARKERS[2])
         # Positive DM tuple
-        self.assertEqual(constants.StrFmt.dmsString((15, 30, 1)), 
+        self.assertEqual(StrFmt.dmsString((15, 30, 1)), 
                          "15"+constants.DEGREE_MARKER+" 30"+constants.ANGLE_MARKERS[1])
         # Negative DM tuple
-        self.assertEqual(constants.StrFmt.dmsString((15, 30, -1)), 
+        self.assertEqual(StrFmt.dmsString((15, 30, -1)), 
                          "-15"+constants.DEGREE_MARKER+" 30"+constants.ANGLE_MARKERS[1])
+
+def suite():
+    """
+    Return a test suite consisting of all the test cases in the module.
+    """
+    
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(StringFormatTest))
+    return theSuite
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    unittest.main(defaultTest='suite')
