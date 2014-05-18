2ebd026e68707b3c43c9a3ed06a8a723f9f947fe
Michael Reuter <mareuternh@gmail.com>
2014-05-18 10:22:54 -0400

    Refs #11. Fixing test to correct time.

--- tests/utils/test_moon_info.py
+++ tests/utils/test_moon_info.py
@@ -13,20 +13,21 @@ class MoonInfoTestCase(unittest.TestCase):
 
     def setUp(self):
         obs = ephem.Observer()
-        obs.date = ephem.Date('2013/10/18 18:00:00')
+        # This is October 18, 2013 6 PM EDT
+        obs.date = ephem.Date('2013/10/18 22:00:00')
         self.moon = MoonInfo()
         self.moon.compute(obs)
 
     def test_age(self):
         age = self.moon.age()
-        self.assertEquals(age, "13.73")
+        self.assertEquals(age, "13.89")
         
     def test_next_four_phases(self):
         next_phases = self.moon.findNextFourPhases()
-        real_phases = [("full", 41564.48448662138), 
-                       ("tq", 41572.486443861846), 
-                       ("new", 41580.034697486895), 
-                       ("fq", 41586.748037173646)]
+        real_phases = [("full", 41564.48448662116), 
+                       ("tq", 41572.486443861955), 
+                       ("new", 41580.03469748699), 
+                       ("fq", 41586.74803717344)]
         
         self.assertEquals(next_phases, real_phases)
 
