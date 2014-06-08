19679edd4500320aac5fab81fc901fc098339466
Michael Reuter <mareuternh@gmail.com>
2014-06-07 21:14:07 -0400

    Refs #13. Forgot to add license headers to some files.

--- lct/features/lunar_feature_container.py
+++ lct/features/lunar_feature_container.py
@@ -1,8 +1,9 @@
-'''
-Created on Jun 11, 2012
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# Copyright (c) 2012-2014, Michael Reuter
+# Distributed under the MIT License. See LICENSE.txt for more information.
+#------------------------------------------------------------------------------
 
-@author: Michael Reuter
-'''
 import sqlite3
 from PyQt4 import QtCore
 
